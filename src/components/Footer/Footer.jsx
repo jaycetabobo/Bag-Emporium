2591b9f7e2e26e4bfff1f65aafa20df7c2c57aa2
Jayce Tabobo <tabobo.jayce01@gmail.com>
2024-10-17 10:37:55 +0800

    info change

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -39,8 +39,9 @@ const Footer = () => {
           <Col md={3} sm={5} className="box">
             <h2>Contact Us</h2>
             <ul>
-              <li>Carmen, Cagayan de Oro, Philippines</li>
+              <li>Tubod, lanao del norte</li>
               <li>Email: kegu.dayot.coc@phinmaed.com</li>
+              <li>Phone: 09552935470</li>
             </ul>
           </Col>
         </Row>
